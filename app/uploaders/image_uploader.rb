ba7cd306493d64b418f72d1c3c90f6dbeb495cb0
EC2 Default User <ec2-user@ip-172-31-25-190.us-east-2.compute.internal>
2021-11-18 10:37:41 +0000

    適当なコミットメッセージ

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -12,7 +12,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
+  
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+  
+  def size_range
+    1..10.megabytes
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
